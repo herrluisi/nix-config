68bde5a76e96df9e6ddc3510c56eb3837d08dda8
Nico Felbinger <nico@felbinger.eu>
2025-11-02 13:10:11 +0100

    hm/mail: cleanup and enable thunderbird

--- modules/hm/mail.nix
+++ modules/hm/mail.nix
@@ -1,124 +1,82 @@
+let
+  gmail = {
+    imap = {
+      host = "imap.gmail.com";
+      port = 993;
+      tls.enable = true;
+    };
+    smtp = {
+      host = "smtp.gmail.com";
+      port = 465;
+      tls.enable = true;
+    };
+  };
+  felbinger = {
+    imap = {
+      host = "mail.felbinger.eu";
+      port = 993;
+      tls.enable = true;
+    };
+    smtp = {
+      host = "mail.felbinger.eu";
+      port = 465;
+      tls.enable = true;
+    };
+  };
+  realName = "Luis Herr";
+  signature = {
+    showSignature = "append";
+    text = ''
+      Mit freundlichen Grüßen
+      Luis Herr
+    '';
+  };
+in
 {
-  ...
-}:
-{
+  programs.thunderbird = {
+    enable = true;
+    profiles.default = {
+      isDefault = true;
+      withExternalGnupg = true;
+    };
+  };
+
   accounts.email.accounts = {
     "kontakt@luisherr.de" = {
       primary = true;
       address = "kontakt@luisherr.de";
-      realName = "Luis Herr";
       userName = "kontakt@luisherr.de";
-      imap = {
-        host = "mail.felbinger.eu";
-        port = 993;
-        tls.enable = true;
-      };
-      smtp = {
-        host = "mail.felbinger.eu";
-        port = 465;
-        tls.enable = true;
-      };
-      signature = {
-        showSignature = "append";
-        text = ''
-          Mit freundlichen Grüßen
-          Luis Herr
-        '';
-      };
+      inherit (felbinger) imap smtp;
+      inherit signature realName;
       thunderbird.enable = true;
     };
     "politik@luisherr.de" = {
       address = "politik@luisherr.de";
-      realName = "Luis Herr";
       userName = "politik@luisherr.de";
-      imap = {
-        host = "mail.felbinger.eu";
-        port = 993;
-        tls.enable = true;
-      };
-      smtp = {
-        host = "mail.felbinger.eu";
-        port = 465;
-        tls.enable = true;
-      };
-      signature = {
-        showSignature = "append";
-        text = ''
-          Mit freundlichen Grüßen
-          Luis Herr
-        '';
-      };
+      inherit (felbinger) imap smtp;
+      inherit signature realName;
       thunderbird.enable = true;
     };
     "luis.maximilian.herr@gmail.com" = {
-      primary = true;
       address = "luis.maximilian.herr@gmail.com";
-      realName = "Luis Herr";
       userName = "luis.maximilian.herr@gmail.com";
-      imap = {
-        host = "imap.gmail.com";
-        port = 993;
-        tls.enable = true;
-      };
-      smtp = {
-        host = "smtp.gmail.com";
-        port = 465;
-        tls.enable = true;
-      };
-      signature = {
-        showSignature = "append";
-        text = ''
-          Mit freundlichen Grüßen
-          Luis Herr
-        '';
-      };
+      inherit (gmail) imap smtp;
+      inherit signature realName;
       thunderbird.enable = true;
     };
     "amphomoronal@gmail.com" = {
-      primary = true;
       address = "amphomoronal@gmail.com";
-      realName = "Luis Herr";
       userName = "amphomoronal@gmail.com";
-      imap = {
-        host = "imap.gmail.com";
-        port = 993;
-        tls.enable = true;
-      };
-      smtp = {
-        host = "smtp.gmail.com";
-        port = 465;
-        tls.enable = true;
-      };
-      signature = {
-        showSignature = "append";
-        text = ''
-          Mit freundlichen Grüßen
-          Luis Herr
-        '';
-      };
+      inherit (gmail) imap smtp;
+      inherit signature realName;
       thunderbird.enable = true;
     };
     "luis@novacodes.eu" = {
       address = "luis@novacodes.eu";
-      realName = "Luis Herr";
       userName = "luis@novacodes.eu";
-      imap = {
-        host = "mail.felbinger.eu";
-        port = 993;
-        tls.enable = true;
-      };
-      smtp = {
-        host = "mail.felbinger.eu";
-        port = 465;
-        tls.enable = true;
-      };
-      signature = {
-        showSignature = "append";
-        text = ''
-          Mit freundlichen Grüßen
-          Luis Herr
-        '';
-      };
+      inherit (felbinger) imap smtp;
+      inherit signature realName;
       thunderbird.enable = true;
     };
+  };
 }
