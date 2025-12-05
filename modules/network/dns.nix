ac128ece897d9cbfb9801bb030123163de50797d
Luis Herr <kontakt@luisherr.eu>
2025-12-05 06:43:02 +0100

    feat(network): changed dns resolver to resolvd

--- modules/network/dns.nix
+++ modules/network/dns.nix
@@ -1,3 +1,10 @@
+{ lib, ... }:
 {
   networking.nameservers = [ "1.1.1.1" ];
+  services.resolved = {
+    enable = true;
+    llmnr = "false";
+    dnsovertls = "opportunistic"; # use dns over tls if available
+    fallbackDns = lib.mkForce [ ]; # prevents split dns with vpn, so we don't want this
+  };
 }
