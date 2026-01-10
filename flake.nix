629e556505936c629d15cd0ede328be1faad22fa
Luis Herr <kontakt@luisherr.eu>
2026-01-11 00:26:11 +0100

    update(home-manager): updated home-manager version to 25.11

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
     };
     nixos-facter-modules.url = "github:numtide/nixos-facter-modules";
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.05";
+      url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nix-index-database = {
