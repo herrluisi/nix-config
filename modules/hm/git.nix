7b29fb4e66af7a3a191b2cf5edb1892501e2aeaf
Luis Herr <kontakt@luisherr.eu>
2026-01-11 00:32:53 +0100

    chore(git): renamed git configs to new git config naming

--- modules/hm/git.nix
+++ modules/hm/git.nix
@@ -4,16 +4,17 @@
     git = {
       enable = true;
 
-      userName = "Luis Herr";
-      userEmail = "kontakt@luisherr.eu";
-
       signing = {
         key = "4790A5119BC18FD4";  # Key from Framework Laptop
         # key = "8A07C1FC53085B285661AF54F2C298803429EA24";  # Key from Acer Nitro 5
         signByDefault = true;
       };
 
-      extraConfig = {
+      settings = {
+        user = {
+          userName = "Luis Herr";
+          userEmail = "kontakt@luisherr.eu";
+        };
         push.autoSetupRemote = true;
         init.defaultBranch = "main";
         safe.directory = "*";
