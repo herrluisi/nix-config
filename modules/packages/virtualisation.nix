9e9e991ff27ded0728dff00e813df89db4e15099
Luis Herr <kontakt@luisherr.eu>
2026-01-11 18:56:09 +0100

    remove(kvm): removed kvm features

--- modules/packages/virtualisation.nix
+++ modules/packages/virtualisation.nix
@@ -1,12 +1,7 @@
 { pkgs, ... }:
 {
   virtualisation = {
-    libvirtd.enable = true;
-    docker.enable = true;
-    waydroid.enable = true;
+   docker.enable = true;
+   waydroid.enable = true;
   };
-
-  programs.virt-manager.enable = true;
-
-  users.users.uisl.extraGroups = [ "libvirtd" ];
 }
