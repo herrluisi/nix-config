3f5b3f45b93a8e877298d53f9ae51276e09b22e7
Luis Herr <kontakt@luisherr.eu>
2026-02-03 12:04:57 +0100

    chore(network-resolve): renamed options due to renaming

--- modules/network/dns.nix
+++ modules/network/dns.nix
@@ -3,8 +3,10 @@
   networking.nameservers = [ "1.1.1.1" ];
   services.resolved = {
     enable = true;
-    llmnr = "false";
-    dnsovertls = "opportunistic"; # use dns over tls if available
-    fallbackDns = lib.mkForce [ ]; # prevents split dns with vpn, so we don't want this
+    settings.Resolve = {
+      LLMNR = "false";
+      DNSOverTLS = "opportunistic"; # use dns over tls if available
+      FallbackDNS = lib.mkForce [ ]; # prevents split dns with vpn, so we don't want this
+    };
   };
 }
