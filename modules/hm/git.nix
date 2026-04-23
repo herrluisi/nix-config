8c45ab0b33029e2901c7e10283565c52ff9892a8
Luis Herr <kontakt@luisherr.eu>
2026-04-23 08:44:33 +0200

    fix: fixed git file

--- modules/hm/git.nix
+++ modules/hm/git.nix
@@ -1,11 +1,10 @@
-{ pkgs, ... }:
 {
   programs = {
     git = {
       enable = true;
 
       signing = {
-        format = "gpg";
+        format = "openpgp";
         key = "4790A5119BC18FD4";  # Key from Framework Laptop
         # key = "8A07C1FC53085B285661AF54F2C298803429EA24";  # Key from Acer Nitro 5
         signByDefault = true;
