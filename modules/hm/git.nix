22150e2019da99e9bc30cc83a60caf45d211149d
Luis Herr <kontakt@luisherr.eu>
2026-04-23 08:41:07 +0200

    chore: reverted new github default signing format

--- modules/hm/git.nix
+++ modules/hm/git.nix
@@ -5,6 +5,7 @@
       enable = true;
 
       signing = {
+        format = "gpg";
         key = "4790A5119BC18FD4";  # Key from Framework Laptop
         # key = "8A07C1FC53085B285661AF54F2C298803429EA24";  # Key from Acer Nitro 5
         signByDefault = true;
