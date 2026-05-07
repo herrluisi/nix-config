8e2d6cb3495ea26678c32d6c985c6b152571f292
Luis Herr <kontakt@luisherr.eu>
2026-05-07 08:34:12 +0200

    chore: aligned zfs forceImportRoot with new standards

--- hosts/uisl/hardware-configuration.nix
+++ hosts/uisl/hardware-configuration.nix
@@ -12,6 +12,8 @@
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
+  
+  boot.zfs.forceImportRoot = false;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
