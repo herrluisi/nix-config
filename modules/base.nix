1d490fe1af89d767959651d190602950e817df06
Luis Herr <kontakt@luisherr.eu>
2026-05-23 09:03:11 +0200

    fix: disables ldap checks because of weird behavior at system building

--- modules/base.nix
+++ modules/base.nix
@@ -11,4 +11,13 @@
   ];
 
   home-manager.backupFileExtension = ".nixbak";
+
+  # disables openldap checks to use lutris. OpenLDAP checks are weird at the building stage from the system.
+  nixpkgs.overlays = [
+    (final: prev: {
+      openldap = prev.openldap.overrideAttrs (old: {
+        doCheck = false;
+      });
+    })
+  ];
 }
