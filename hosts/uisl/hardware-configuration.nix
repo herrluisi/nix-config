b9916b3ad53768e204454a95304b46d048a3143f
Luis Herr <kontakt@luisherr.eu>
2026-07-14 17:20:44 +0200

    chore: cleanup code

--- hosts/uisl/hardware-configuration.nix
+++ hosts/uisl/hardware-configuration.nix
@@ -1,17 +1,19 @@
-{ config, lib, pkgs, modulesPath, ... }:
+{ config, lib, modulesPath, ... }:
 
 {
   imports =
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "thunderbolt" "usbhid" ];
-  boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-amd" ];
-  boot.extraModulePackages = [ ];
-
-  boot.zfs.forceImportRoot = false;
-
+  boot = {
+    initrd = { 
+      availableKernelModules = [ "nvme" "xhci_pci" "thunderbolt" "usbhid" ];
+      kernelModules = [ ];
+    };
+    kernelModules = [ "kvm-amd" ];
+    extraModulePackages = [ ];
+    zfs.forceImportRoot = false;
+  };
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
