c5fbfd0c0edd992c45ffb5780ba2c3f6f7a184e4
Luis Herr <kontakt@luisherr.eu>
2026-08-08 23:24:19 +0200

    chore: changed kernelVersion

--- hosts/uisl/hardware-configuration.nix
+++ hosts/uisl/hardware-configuration.nix
@@ -13,9 +13,11 @@
     kernelModules = [ "kvm-amd" ];
     extraModulePackages = [ ];
     zfs.forceImportRoot = false;
-    kernelPackages = pkgs.linuxPackages;
+    kernelPackages = pkgs.linuxPackages_6_12;
   };
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+
+  hardware.enableRedistributableFirmware = true;
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 
   services.fwupd.enable = true;
